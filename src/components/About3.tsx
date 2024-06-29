69966790bcd8149b7aefc1a26856df5f9f5207c8
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-06-29 23:05:40 +0530

    client added

--- src/components/About3.tsx
+++ src/components/About3.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Mail, MapPin } from "lucide-react";
 import { useEffect, useState } from "react";
 
--- src/components/About4.tsx
+++ src/components/About4.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Progress } from "@radix-ui/react-progress";
 import { ChevronDown, ChevronUp, GraduationCap } from "lucide-react";
 import { useEffect, useState } from "react";
